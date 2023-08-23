91fb44c3543b32aa739d8ea92ec6ae75857147bb
Mouad <elbachalimouad@gmail.com>
2023-08-23 17:25:19 +0100

    first commit

---
+++ intellcapstg/stagiaire/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path ,include
+from . import views 
+
+urlpatterns=[
+    path('',views.index,name='index'),
+
+     path('about',views.about,name='about'),
+
+    path('contact',views.contact,name='contact'),
+
+    path('signin',views.signin,name='signin'),
+
+    path('signup',views.signup,name='signup'),
+
+    path('activitemain',views.activitemain,name='activitemain'),
+
+    path('document',views.document, name='document'),
+
+    path('forum',views.forum,name='forum'),
+
+    path('postuler',views.postuler,name='postuler'),
+
+
+    path('offre',views.offre,name='offre'),
+
+    path('profile',views.profile, name='profile'),
+
+    path('profileinfo',views.profileinfo, name='profileinfo'),
+
+    path('profilepass',views.profilepass, name='profilepass'),
+
+
+    path('search',views.search, name='search'),]
+
+
+
+
+
+
+
---
+++ intellcapstg/stagiaire/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def about(request):
+    return render(request,'stagiaire/about.html')
+
+
+def activitemain(request):
+    return render(request, 'stagiaire/activitemain.html')
+
+
+def contact(request):
+    return render(request,'stagiaire/contact.html')
+
+
+
+
+
+def document(request):
+    return render(request, 'stagiaire/document')
+
+
+
+def forum(request):
+    return render(request, 'stagiaire/forum.html')
+
+
+def index(request):
+    return render(request, 'stagiaire/index.html')
+
+
+
+def offre(request):
+
+    return render(request, 'stagiaire/offre.html')
+
+
+def postuler(request):
+    return render(request,'stagiaire/postuler.html')
+
+
+
+
+
+def profile(request):
+
+    return render(request, 'stagiaire/profile.html')
+
+
+
+
+def profileinfo(request):
+    return render(request,'stagiaire/profileinfo.html')
+
+
+
+
+def profilepass(request):
+
+    return render(request, 'stagiaire/profilepass.html')
+
+
+
+def search(request):
+    return render(request, 'stagiaire/search.html')
+
+
+
+def signup(request):
+     return render (request,'stagiaire/signup.html')
+
+
+def  signin(request):
+    return render (request, '/stagiaire/signin.html')
+
+
+
+
+
+
+
+
+
